476c101b3c9d2edd86fc769dd4ba2cab710c615f
Andrew Blomenberg <andrewBlomen@gmail.com>
2017-11-02 14:17:54 -0400

    Quick bug fix from last commit

--- assembler.py
+++ assembler.py
@@ -3,19 +3,13 @@ from enum import Enum
 
 class Label(object):
     def __init__(self):
-        self.indicies = []  # Instructions will be stored in a list. This is the indicies of that list that need this label
+        self.indices = []  # Instructions will be stored in a list. This is the indicies of that list that need this label
         self.location = -1  # The location that the label references
-
-
-class OpType(Enum):
-    r = 1
-    i = 2
-    j = 3
 
 
 class Operation(object):
 
-    def __init__(self, opType, opCode, funCode = -1):
+    def __init__(self, opType, opCode, funCode=-1):
         self.opType = opType
         self.opCode = opCode
         self.funCode = funCode
@@ -26,19 +20,19 @@ def opDictFromFile(fName):
     opDict = {}
     for line in inFile:
         lineList=line.split()
-        lineList[1] = OpType(lineList[1])  # operation type (j,i,r)
-        lineList[2] = int(lineList[2], 2)  # opcode
 
-        if len(lineList) > 3:
-            lineList[3] = int(lineList[3], 2)  # funcode
-        else:
-            lineList.append("")
-            lineList[3] = -1
+        if len(lineList)>0:
+            lineList[1] = lineList[1]  # operation type (j,i,r)
+            lineList[2] = int(lineList[2], 2)  # opcode
 
-        opDict[lineList[0]] = Operation(lineList[1], lineList[2], lineList[3])
+            if len(lineList) > 3:
+                lineList[3] = int(lineList[3], 2)  # funcode
+            else:
+                lineList.append("")
+                lineList[3] = -1
+
+            opDict[lineList[0]] = Operation(lineList[1], lineList[2], lineList[3])
 
     return opDict
 
-
-print(opDictFromFile("operations.txt"))
 
